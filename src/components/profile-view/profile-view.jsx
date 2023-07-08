fd924d95481f057f34a9d6fc2d5cd70039485b94
ddecicco330web <ddecicco330@gmail.com>
2023-07-07 20:31:15 -0400

    Refactor Remove favorite movie

--- src/components/profile-view/profile-view.jsx
+++ src/components/profile-view/profile-view.jsx
@@ -1,38 +1,28 @@
 import { useParams } from 'react-router';
 import { Card, Button, Col, Row } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
-import { useEffect } from 'react';
-import { DeleteUser } from '../../services/api-calls';
+import { DeleteUser, RemoveFromFavorites } from '../../services/api-calls';
 import { MovieCard } from '../movie-card/movie-card';
 
-export const ProfileView = ({
-  users,
-  resetChanges,
-  onDelete,
-  token,
-  movies
-}) => {
-  if (users.length === 0) {
+export const ProfileView = ({ user, onDelete, token, movies, setUser }) => {
+  const { username } = useParams();
+  if (!user) {
     return;
   }
-  const { username } = useParams();
-  const user = users.find((arrUser) => arrUser.username === username);
-
-  useEffect(() => resetChanges(user), []);
-
-  let date = new Date(user.birthday);
+  console.log(user);
+  let date = new Date(user.Birthday);
 
   let dateString = new Date(date.getTime()).toISOString().split('T')[0];
 
-  let favoriteMovies = movies.filter((m) => user.favoriteMovies.includes(m.id));
+  let favoriteMovies = movies.filter((m) => user.FavoriteMovies.includes(m.id));
 
-  return (
+  return username === user.Username ? (
     <>
       <Card bg="secondary">
         <Card.Title className="mt-1 ms-1">
-          Username: {user.username}{' '}
+          Username: {user.Username}{' '}
         </Card.Title>
-        <Card.Text className="ms-1 mb-1">Email: {user.email}</Card.Text>
+        <Card.Text className="ms-1 mb-1">Email: {user.Email}</Card.Text>
         <Card.Text className="ms-1">Birthday: {dateString}</Card.Text>
         <Button
           className="ms-1 me-1"
@@ -56,7 +46,7 @@ export const ProfileView = ({
           <h2>Favorite Movies:</h2>
         </Col>
       </Row>
-      <Row classname="justify-content-md-center">
+      <Row>
         {favoriteMovies.map((movie) => {
           return (
             <Col className="mb-5 mt-1" key={movie.id} md={3}>
@@ -64,26 +54,7 @@ export const ProfileView = ({
               <Button
                 className="mt-1"
                 onClick={() => {
-                  fetch(
-                    `https://my-flix330.herokuapp.com/users/${encodeURIComponent(
-                      username
-                    )}/movies/${encodeURIComponent(movie.id)}`,
-                    {
-                      method: 'DELETE',
-                      headers: {
-                        Authorization: `Bearer ${token}`
-                      }
-                    }
-                  )
-                    .then((response) => {
-                      if (response.ok) {
-                        alert('Movie removed');
-                        window.location.reload();
-                      } else {
-                        alert('Failed to remove movie');
-                      }
-                    })
-                    .catch((e) => console.log(e));
+                  RemoveFromFavorites(user, movie, token, setUser);
                 }}
               >
                 Remove
@@ -93,5 +64,7 @@ export const ProfileView = ({
         })}
       </Row>
     </>
+  ) : (
+    <Row>Invalid username</Row>
   );
 };
